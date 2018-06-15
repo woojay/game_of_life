d441e4d219a345cbe7ea05066eaae9722be84780
woojay <fullyanalog-programmer@yahoo.com>
2018-06-14 19:10:49 -0700

    ^ README and instructions

--- README.md
+++ README.md
@@ -18,13 +18,13 @@ Users may define the appearance of live and dead cells.
 - Game Play
 0.  I used pyenv to test py 3.6.4 and py 2.7.14 environements, FYI
 1.  Enter a single character to represent a live cell. Any alpha-numeric is great.
-2.  Enter a single character to represent a dead cell.  '.' works well.
-3.  Highly recommend to enter '0' to select manually.  Selecting between 1-9 is highly likely to all die in the
-    first round.
-4.  10x40 grid will show up, which is the 'universe.'  You can move around the position w/ a, d, w, and x keys.
-5.  Once located, enter 's' to place a seed.  Do as many as you like, and enter 'q' to exit the set up mode.
-6.  Once exited, the each round is run after a single keyboard input other than 'x'.  Each key press will represent
+2.  Enter a single character to represent a dead cell.  ' ' or '.' works well.
+3.  Enter '0' to select manual seed entry or select between 1-9 to randomly place 10 to 90 seeds.
+4.  For manual entry, 10x40 grid will show up, which is the 'universe.'  You can move around the cursor position with arrow keys.
+    Hit space to place a seed.  Do as many as you like, and hit END key to exit the set up mode.
+5.  Once set up, the each round is run after a single keyboard input other than 'x'.  Each key press will represent
     a single round.
-7.  If all the cells die, 'game over' message will show, but will not exit unless a single 'x' is entered.
-8.  'x' will always exit the simulation rounds.
-9.  In case of a 'still life,' no special message will show even though each round will return a same outcome.
+6.  If all the cells die, a message will show, but will not exit unless a single 'x' is entered.
+7.  'x' will always exit the simulation rounds.
+8.  In case of a 'still life,' no special message will show even though each round will return a same outcome.
+9.  Hope this makes sense to you.  Thank you so much.
--- game_of_life.py
+++ game_of_life.py
@@ -19,20 +19,20 @@
     0.  '(sudo) pip install curses' if needed
     1.  'python game_of_life.py' in either python 3.6.4 or 2.7.14 (linux only)
 
-    - Game Play
+    - Game Playl
     0.  I used pyenv to test py 3.6.4 and py 2.7.14 environements, FYI
     1.  Enter a single character to represent a live cell. Any alpha-numeric is great.
-    2.  Enter a single character to represent a dead cell.  '.' works well.
-    3.  Highly recommend to enter '0' to select manually.  Selecting between 1-9 is highly likely to all die in the
-        first round.
-    4.  10x40 grid will show up, which is the 'universe.'  You can move around the position w/ a, d, w, and x keys.
-    5.  Once located, enter 's' to place a seed.  Do as many as you like, and enter 'q' to exit the set up mode.
-    6.  Once exited, the each round is run after a single keyboard input other than 'x'.  Each key press will represent
+    2.  Enter a single character to represent a dead cell.  ' ' or '.' works well.
+    3.  Enter '0' to select manual seed entry or select between 1-9 to randomly place 10 to 90 seeds.
+    4.  For manual entry, 10x40 grid will show up, which is the 'universe.'  You can move around the cursor position
+            with arrow keys.
+        4.1.  Hit space to place a seed.  Do as many as you like, and hit END key to exit the set up mode.
+    5.  Once set up, the each round is run after a single keyboard input other than 'x'.  Each key press will represent
         a single round.
-    7.  If all the cells die, 'game over' message will show, but will not exit unless a single 'x' is entered.
-    8.  'x' will always exit the simulation rounds.
-    9.  In case of a 'still life,' no special message will show even though each round will return a same outcome.
-    10. Hope this makes sense to you.  Thank you so much.
+    6.  If all the cells die, a message will show, but will not exit unless a single 'x' is entered.
+    7.  'x' will always exit the simulation rounds.
+    8.  In case of a 'still life,' no special message will show even though each round will return a same outcome.
+    9.  Hope this makes sense to you.  Thank you so much.
 
 
 '''
@@ -253,7 +253,7 @@ def get_manual_seeds(stdscr):
 
     show_universe(stdscr)
 
-    stdscr.addstr(height + 2, 0, 'Use arrow keys to move.  Enter a space to place a seed. Hit END key to finish. ')
+    stdscr.addstr(height + 2, 0, 'Use arrow keys to move the cursor.  Enter a space to place a seed. Hit END key to finish. ')
 
     x = 0
     y = 0
@@ -291,14 +291,11 @@ def get_manual_seeds(stdscr):
             else:
                 y = y + 1
 
-        if chr(key) == ' ': # Place Seed
+        elif chr(key) == ' ': # Place Seed
             stdscr.addstr(y, x, live)
-            stdscr.addstr(y, x, '')     # Moves back the cursor after write
             universe[y][x] = live
 
-        else:
-            stdscr.addstr(y, x, '')
-
+        stdscr.addstr(y, x, '')
         stdscr.refresh()
 
 
@@ -339,7 +336,7 @@ def get_seeds(stdscr):
 def clear_display(stdscr):
     '''
     Clears display window
-\    '''
+    '''
 
     height, width = stdscr.getmaxyx()
 
